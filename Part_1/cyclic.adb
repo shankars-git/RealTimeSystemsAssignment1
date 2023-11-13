e5c83c152f3b0fd035b711ebbb5e7fe317fdfbaa
Max Dahlgren <maxedahlgren@gmail.com>
2023-11-13 18:54:06 +0100

    Auto format part 1 and 2

--- Part_1/cyclic.adb
+++ Part_1/cyclic.adb
@@ -4,43 +4,42 @@ use Ada.Calendar;
 use Ada.Text_IO;
 
 procedure cyclic is
-    Message: constant String := "Cyclic scheduler";
+    Message        : constant String := "Cyclic scheduler";
     -- change/add your declarations here
-    d: Duration := 1.0;
-	Start_Time: Time := Clock;
-	Execution_Time : Time;
-	s: Integer := 0;
-        
+    d              : Duration        := 1.0;
+    Start_Time     : Time            := Clock;
+    Execution_Time : Time;
+    s              : Integer         := 0;
 
-	procedure f1 is 
-		Message: constant String := "f1 executing, time is now";
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-	end f1;
+    procedure f1 is
+        Message : constant String := "f1 executing, time is now";
+    begin
+        Put (Message);
+        Put_Line (Duration'Image (Clock - Start_Time));
+    end f1;
 
-	procedure f2 is 
-		Message: constant String := "f2 executing, time is now";
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-	end f2;
+    procedure f2 is
+        Message : constant String := "f2 executing, time is now";
+    begin
+        Put (Message);
+        Put_Line (Duration'Image (Clock - Start_Time));
+    end f2;
 
-	procedure f3 is 
-		Message: constant String := "f3 executing, time is now";
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-	end f3;
+    procedure f3 is
+        Message : constant String := "f3 executing, time is now";
+    begin
+        Put (Message);
+        Put_Line (Duration'Image (Clock - Start_Time));
+    end f3;
 
-	begin
-        loop
-            -- change/add your code inside this loop   
-			    Execution_Time := Clock; 
-                f1;
-				f2;
-				delay until Execution_Time + 0.5;
-				f3;
-				delay until Execution_Time + d;
-        end loop;
+begin
+    loop
+        -- change/add your code inside this loop
+        Execution_Time := Clock;
+        f1;
+        f2;
+        delay until Execution_Time + 0.5;
+        f3;
+        delay until Execution_Time + d;
+    end loop;
 end cyclic;
--- Part_2/cyclic_wd.adb
+++ Part_2/cyclic_wd.adb
@@ -1,4 +1,4 @@
---Cyclic scheduler with a watchdog: 
+--Cyclic scheduler with a watchdog:
 
 with Ada.Calendar;
 with Ada.Text_IO;
@@ -6,90 +6,94 @@ with Ada.Numerics.Float_Random;
 
 use Ada.Calendar;
 use Ada.Text_IO;
-Use Ada.Numerics.Float_Random;
+use Ada.Numerics.Float_Random;
 
 -- add packages to use randam number generator
 
-
 procedure cyclic_wd is
-    Message: constant String := "Cyclic scheduler with watchdog";
+    Message        : constant String   := "Cyclic scheduler with watchdog";
     -- change/add your declarations here
-    RanGen : Generator;
-    d: Duration := 1.0;
-	Start_Time: Time := Clock;
-	Execution_Time : Time;
-	End_Time : Time;
-	F3TaskDeadline: constant Duration := 0.5;
-    Wdg_Start_Time: Time;
+    RanGen         : Generator;
+    d              : Duration          := 1.0;
+    Start_Time     : Time              := Clock;
+    Execution_Time : Time;
+    End_Time       : Time;
+    F3TaskDeadline : constant Duration := 0.5;
+    Wdg_Start_Time : Time;
 
-	procedure f1 is 
-		Message: constant String := "f1 executing, time is now";
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-	end f1;
+    procedure f1 is
+        Message : constant String := "f1 executing, time is now";
+    begin
+        Put (Message);
+        Put_Line (Duration'Image (Clock - Start_Time));
+    end f1;
 
-	procedure f2 is 
-		Message: constant String := "f2 executing, time is now";
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-	end f2;
+    procedure f2 is
+        Message : constant String := "f2 executing, time is now";
+    begin
+        Put (Message);
+        Put_Line (Duration'Image (Clock - Start_Time));
+    end f2;
 
-	procedure f3 is 
-		Message: constant String := "f3 executing, time is now";
-		Random_Generated_Delay: Duration;
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-		Reset (RanGen);
-		Random_Generated_Delay := Duration(Float(Random(RanGen)));
+    procedure f3 is
+        Message : constant String := "f3 executing, time is now";
+        Random_Generated_Delay : Duration;
+    begin
+        Put (Message);
+        Put_Line (Duration'Image (Clock - Start_Time));
+        Reset (RanGen);
+        Random_Generated_Delay := Duration (Float (Random (RanGen)));
         delay until Clock + Random_Generated_Delay;
-	end f3;
-	
-	task Watchdog is
-	    -- add your task entries for communication 	
-        entry Start_Wdg;
-		entry Stop_wdg;
-	end Watchdog;
+    end f3;
 
-  task body Watchdog is
+    task Watchdog is
+        -- add your task entries for communication
+        entry Start_Wdg;
+        entry Stop_wdg;
+    end Watchdog;
+
+    task body Watchdog is
     begin
         loop
-				accept Start_Wdg;
-				Wdg_Start_Time := Clock;
-				select
-					accept Stop_wdg do
-						Put_Line("Watchdog: f3 finished, in " & Duration'Image(Clock - Wdg_Start_Time));
-					end Stop_wdg;
-			or
-				delay until Clock + 0.5;
-				Put_Line("Watchdog: Timeout for Procedure f3, it is exceeding the deadline");
-				accept Stop_wdg do
-						Put_Line("Watchdog: f3 finished, in " & Duration'Image(Clock - Wdg_Start_Time));
-				end Stop_wdg;
-			end select;
+            accept Start_Wdg;
+            Wdg_Start_Time := Clock;
+            select
+                accept Stop_wdg do
+                    Put_Line
+                       ("Watchdog: f3 finished, in " &
+                        Duration'Image (Clock - Wdg_Start_Time));
+                end Stop_wdg;
+            or
+                delay until Clock + 0.5;
+                Put_Line
+                   ("Watchdog: Timeout for Procedure f3, it is exceeding the deadline");
+                accept Stop_wdg do
+                    Put_Line
+                       ("Watchdog: f3 finished, in " &
+                        Duration'Image (Clock - Wdg_Start_Time));
+                end Stop_wdg;
+            end select;
         end loop;
     end Watchdog;
 
-	begin
-        loop
-			  Execution_Time := Clock;
-			  End_Time := Clock +d;   
-              f1;
-              f2;
-			  delay until Execution_Time + 0.5;
-			  -- Start Watchdog
-			  Watchdog.Start_Wdg;
-		      f3;
-			  -- Stop watchdog
-			  Watchdog.Stop_wdg;     
-			if Clock > End_Time then
-						loop
-							End_Time := End_Time + d;
-							exit when CLock < End_Time;
-						end loop;
-					end if;
-			delay until End_Time;       
-		 end loop;
+begin
+    loop
+        Execution_Time := Clock;
+        End_Time       := Clock + d;
+        f1;
+        f2;
+        delay until Execution_Time + 0.5;
+        -- Start Watchdog
+        Watchdog.Start_Wdg;
+        f3;
+        -- Stop watchdog
+        Watchdog.Stop_wdg;
+        if Clock > End_Time then
+            loop
+                End_Time := End_Time + d;
+                exit when CLock < End_Time;
+            end loop;
+        end if;
+        delay until End_Time;
+    end loop;
 end cyclic_wd;
